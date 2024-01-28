daecd46476dc0977f5b0cd4da371776280cce13b
mblevu <brianislevu@gmail.com>
2024-01-28 21:26:58 +0300

    Add hypermedia pagination functionality

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+""" Simple pagination """
+import csv
+import math
+from typing import List, Tuple
+
+
+def index_range(page, page_size) -> Tuple:
+    """
+    Returns the start and end indices for a given page and page size.
+    """
+    start_index = (page - 1) * page_size
+    if start_index < 0:
+        start_index = 0
+    end_index = page * page_size
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ Finds the correct indexes to paginate dataset correctly """
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
+        start, end = index_range(page, page_size)
+        return self.dataset()[start:end]
+    
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ Finds the correct indexes to paginate dataset correctly """
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
+        data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+        return {
+            'page_size': len(data),
+            'page': page,
+            'data': data,
+            'next_page': next_page,
+            'prev_page': prev_page,
+            'total_pages': total_pages
+        }
