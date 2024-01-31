045a7376c0bf945c10f37f956624ae0f53a21138
mblevu <brianislevu@gmail.com>
2024-01-31 12:40:23 +0300

    Remove unnecessary else statement in BasicCache class

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,39 +1,18 @@
 #!/usr/bin/env python3
 """Basic cache dictionary"""
+BaseCaching = __import__('base_caching').BaseCaching
 
 
-class BaseCaching():
-    """ BaseCaching defines:
-      - constants of your caching system
-      - where your data are stored (in a dictionary)
-    """
-    MAX_ITEMS = 4
-
-    def __init__(self):
-        """ Initiliaze
-        """
-        self.cache_data = {}
-
-    def print_cache(self):
-        """ Print the cache
-        """
-        print("Current cache:")
-        for key in sorted(self.cache_data.keys()):
-            print("{}: {}".format(key, self.cache_data.get(key)))
-
-
-class BasicCache(BaseCaching):
+class BaseCache(BaseCaching):
     """inherits from BaseCaching class"""
     def put(self, key, item):
-        """assigns to the dictionary"""
-        if key and item:
-            self.cache_data[key] = item
-        else:
-            pass
+        """Add an item in the cache"""
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
 
     def get(self, key):
-        """must return the value linked to key"""
-        if key in self.cache_data:
-            return self.cache_data[key]
-        else:
+        """Get an item by key"""
+        if key is None or key not in self.cache_data:
             return None
+        return self.cache_data[key]
