cd112fc9dd3568dbb2cc84c4f13bbfb2e5874dc6
mblevu <brianislevu@gmail.com>
2024-02-04 14:35:59 +0300

    Fix typo in LIFOCache put method

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -8,13 +8,12 @@ class LIFOCache(BaseCaching):
     """lifo caching module that imports from basecaching"""
     def __init__(self):
         super().__init__()
-        self.cache_data = {}
 
     def put(self, key, item):
         """adds an item in the cache"""
         if key is None or item is None:
             return
-        self.cache_dat[key] = item
+        self.cache_data[key] = item
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             last = next(reversed(self.cache_data))
             self.cache_data.pop(last)
