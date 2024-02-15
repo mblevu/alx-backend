86f79feac240b22980cb32e8d30af3b74cd94caf
mblevu <brianislevu@gmail.com>
2024-02-15 11:56:56 +0300

    Refactor render_template arguments in index function

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,7 +8,9 @@ app = Flask(__name__)
 @app.route('/')
 def index() -> str:
     """index page"""
-    return render_template('0-index.html', title='Welcome to Holberton', text='Hello world')
+    return render_template('0-index.html',
+                           title='Welcome to Holberton',
+                           text='Hello world')
 
 
 if __name__ == '__main__':
