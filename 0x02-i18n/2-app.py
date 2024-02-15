3de8c48404c6c35f97421e6dc72bd96a9c89a5cd
mblevu <brianislevu@gmail.com>
2024-02-15 13:43:27 +0300

    Add Flask app with internationalization support

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """flask app"""
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 app = Flask(__name__)
@@ -20,9 +20,15 @@ app.config.from_object(Config)
 @app.route('/')
 def index() -> str:
     """index page"""
-    return render_template('1-index.html',
+    return render_template('2-index.html',
                            title='Welcome to Holberton',
                            text='Hello world')
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """get locale to determine best match for language"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == '__main__':
