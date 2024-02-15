b37d10eeeaa196f0a0250831589aa8c345b32bdd
mblevu <brianislevu@gmail.com>
2024-02-15 11:39:54 +0300

    flask app to render html page

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,17 +1,14 @@
-#!/usr/bin/env python3
-"""basic flask app"""
-from flask import Flask, render_template, request
-from flask_babel import Babel, gettext, format_datetime
-from datetime import datetime
+#/usr/bin/env python3
+"""flask app"""
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    """index page"""
     return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
