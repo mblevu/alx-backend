774b7d849a5e7948f8599717bcd6bab20cb12459
mblevu <brianislevu@gmail.com>
2024-03-05 23:29:08 +0300

    Add Redis advanced operations script

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,19 @@
+const redis = require('redis');
+
+const client = redis.createClient();
+
+client.on('error', (error) => {
+    console.error(error);
+});
+
+client.hset('HolbertonSchools', 'Portland', 50, redis.print);
+client.hset('HolbertonSchools', 'Seattle', 80, redis.print);
+client.hset('HolbertonSchools', 'New York', 20, redis.print);
+client.hset('HolbertonSchools', 'Bogota', 20, redis.print);
+client.hset('HolbertonSchools', 'Cali', 40, redis.print);
+client.hset('HolbertonSchools', 'Paris', 2, redis.print);
+
+client.hgetall('HolbertonSchools', (err, reply) => {
+    console.log(reply);
+});
+client.quit();
