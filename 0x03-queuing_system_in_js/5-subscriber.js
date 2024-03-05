13a6663ee88cef24d4b05b9b25f995276e3bdd40
mblevu <brianislevu@gmail.com>
2024-03-05 23:39:57 +0300

    Add publisher and subscriber scripts for queuing system in JavaScript

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,21 @@
+const { channel } = require('diagnostics_channel');
+const redis = require('redis');
+const client = redis.createClient();
+
+client.on('connect', () => {
+    console.log('Redis client connected to the server');
+});
+
+client.on('error', (error) => {
+    console.error(`Redis client not connected to the server: ${error.message}`);
+});
+
+client.subscribe('holberton school channel');
+
+client.on('message', (channel, message) => {
+    console.log(message);
+    if (message === 'KILL_SERVER') {
+        client.unsubscribe();
+        client.quit();
+    }
+});
